e5cbbbd4ec31cdc59e2660c8752229fb1003891a
DeepeshKalura <deepeshkalurs@gmail.com>
2025-09-19 21:46:07 +0530

    feat: added user things in the database man

--- packages/db/schema.ts
+++ packages/db/schema.ts
@@ -1,22 +1,33 @@
 import {
-  pgTable,
-  serial,
-  text,
-  timestamp,
-  varchar,
+	pgTable,
+	serial,
+	text,
+	timestamp,
+	varchar,
 } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 
 // Campaigns Table
 export const campaigns = pgTable("campaigns", {
-  id: serial("id").primaryKey(),
-  title: varchar("title", { length: 256 }).notNull(),
-  description: text("description").notNull(),
-  imageUrl: varchar("image_url", { length: 1024 }),
-  createdAt: timestamp("created_at").defaultNow().notNull(),
-  updatedAt: timestamp("updated_at")
-    .defaultNow()
-    .$onUpdate(() => new Date()),
+	id: serial("id").primaryKey(),
+	title: varchar("title", { length: 256 }).notNull(),
+	description: text("description").notNull(),
+	imageUrl: varchar("image_url", { length: 1024 }),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+});
+
+// users 
+
+export const users = pgTable("users", {
+	id: serial("id").primaryKey(),
+	createdAt: timestamp("created_at").defaultNow().notNull(),
+	updatedAt: timestamp("updated_at")
+		.defaultNow()
+		.$onUpdate(() => new Date()),
+
 });
 
 // Zod schema for validation
